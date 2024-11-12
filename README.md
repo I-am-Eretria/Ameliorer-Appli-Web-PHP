9270e46e2ee2a99576a97260a58be99e1dec733f
I-am-Eretria <marine.ledemeney@gmail.com>
2024-11-12 15:06:27 +0100

    Premier Dépôt

---
+++ README.md
@@ -0,0 +1,48 @@
+# Améliorer l'Application Web PHP 💻 #
+## - Apporter plusieurs fonctionnalités supplémentaires - ##
+
+<br>
+
+
+### Objectifs : ###
+***Apporter plusieurs fonctionnalités supplémentaires à l'application.***
+
+
+*Première partie : <a href="https://github.com/I-am-Eretria/Premiere-Appli-Web-PHP">Première Appli Web PHP</a>*
+
+
+→ Pour cela il faudra : 
+      <ul><ul>
+        <li>améliorer le rendu visuel des pages (ajout CSS dans index.php et recap.php)</li>
+        <br>
+        <li>permettre à l'utilisateur d'aller sur la page recap.php ou index.php à tout moment, à l’aide d’une barre de navigation</li>
+        <br>
+        <li>afficher le nombre de produits présents en session à tout moment, quelle que soit la page 
+        affichée</li>
+        <br>
+        <li>faire en sorte que le fichier traitement.php, lorsqu'il retourne au formulaire, créé un 
+        message (d'erreur ou de succès, selon le cas de figure) et permettre à index.php de l'afficher
+        .</li>
+        <br>
+        <li>Ajouter trois fonctionnalités utiles dans recap.php :</li>
+            <ul>
+                <li>supprimer un produit en session (selon le choix de l'utilisateur)</li>
+                <li>supprimer tous les produits en session en une seule fois</li>
+                <li>modifier les quantités de chaque produit grâce à deux points "+" et "-" positionnés de part et d'autre du nombre dans la cellule</li>
+                <li>ajouter des messages de notifications pour la suppression d’article.</li>
+            </ul>
+        </ul></ul>
+<br>
+
+### ⚙️ Langages utilisés ⚙️ ###
+
+<img src="https://skillicons.dev/icons?i=html,css,php,github"/>
+
+<br>
+<!-- 
+### Preview : ###
+<img src="Preview.gif"></img> -->
+
+<br>
+
+*Note : exercice fourni par <a href="https://elan-formation.fr/accueil">Elan Formation</a>*
---
+++ traitement.php
@@ -0,0 +1,138 @@
+<?php
+
+    // Utilités fonction session_start():  
+    // 1:  démarrer une session pour l'utilisateur courant
+    // 2:  récupérer sa session (si une déjà présente), possible car au démarrage d'une session le serveur enregistrera un cookie PHPSESSID dans le navigateur client 
+    //                                                           -> cookie contient identifiant de la session et sera transmis au serveur avec chaque requête HTTP effectuée par le client                                   
+    session_start();
+
+
+
+    if(isset($_GET['action'])){
+
+        
+        switch($_GET['action']){
+
+
+            case "add": 
+
+                // Pour éviter utilisateur accède directement à URL page traitement
+                // et ainsi provoquer des erreurs  sur  la  page  qui  lui  présenterait  des  informations  que  nous  ne  souhaitons  pas dévoiler
+                //  -> limiter l'accès à traitement.php par  les  seules  requêtes  HTTP provenant de la soumission de notre formulaire
+
+                if(isset($_POST['submit'])){    // vérification clef submit dans tableau post, submit correspond à attribut name du formulaire
+
+
+
+                    // vérification intégrité des valeurs transmises dans le tableau post
+
+                        $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING);
+                        // FILTER_SANITIZE_STRING : supprime présence caractères spéciaux / balise HTML ou encode  -> Pas d'injection de code HTML possible
+
+                        $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+                        // FILTER_VALIDATE_FLOAT :  validera  le  prix  seulement si c'est bien un  nombre  à virgule
+                        // ajout de FILTER_FLAG_ALLOW_FRACTION :  permet l'utilisation du caractère "," ou "." pour la décimale
+                        
+                        $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
+                        // FILTER_VALIDATE_INT : quantité validée  que  si  celle-ci  est  un nombre entier différent de zéro (qui est considéré comme nul)
+
+
+
+                    // vérification pour voir si filtres ont bien fonctionné
+                    if ($name && $price && $qtt){   // on ne les compare à rien de précis car si filtre échoue renverra false ou null
+
+                        // stockage des données en session -> tableau session
+
+                            // tableau associatif product pour chaque produit
+                            $product = [
+                                "name" => $name,
+                                "price" => $price,
+                                "qtt" => $qtt,
+                                "total" => $price*$qtt
+                            ];
+
+
+                        // enregistrement nouveau produit créé en session
+                        $_SESSION['products'][] = $product;
+                            //      clef  (dans tableau session)
+                            //  []  ->  raccourci indiquant à cet emplacement nouvelle entrée au futur tableau products
+                    }
+
+
+                    // Fonction header () : envoie un nouvel entête HTTP (les entêtes d'une réponse) au client
+                    // ici type d'appel "Location" -> réponse envoyée au client avec status code 302 -> redirection
+                    header("Location:index.php");
+
+
+
+                }   // condition true seulement si requête post transmet une clef submit au serveur
+                    // sinon redirection grâce à la fonction header()
+
+
+
+            break;
+
+            
+
+            case "delete": 
+                // fonction unset : détruit la ou les variables
+                // -> ici détruit un produit spécifique (via l'identifiant) du tableau de produits stocké en session
+                unset($_SESSION['products'][$_GET['id']]);
+                header("Location: recap.php");
+
+            break;
+
+           
+            case "clear": 
+
+                // fonction unset : détruit la ou les variables
+                // -> ici détruit le tableau products
+                unset($_SESSION['products']);
+                header("Location: recap.php");
+
+            break;
+
+
+
+            case "up-qtt": 
+
+                // désigne tableau products -> cible product en particulier (id) -> quantité de ce produit -> +1 à quantité actuelle
+                $_SESSION["products"][$_GET['id']]['qtt'] ++;
+                header("Location: recap.php");
+
+            break;
+
+            
+
+            case "down-qtt": 
+
+
+                    if($_SESSION["products"][$_GET['id']]['qtt']>1){
+                        // désigne tableau products -> cible product en particulier (id) -> quantité de ce produit -> -1 à quantité actuelle
+                        $_SESSION["products"][$_GET['id']]['qtt'] --;
+                        header("Location: recap.php");
+                    }
+
+                    elseif($_SESSION["products"][$_GET['id']]['qtt']<=0){
+                        unset($_SESSION['products'][$_GET['id']]);
+                        header("Location: recap.php");
+                    }
+
+            break;
+
+        }
+    }
+
+
+
+
+    // if(isset($_GET)['action']){
+
+        //     switch($_GET['action']){
+        //         case "add": ;
+        //         case "delete": ;
+        //         case "clear": ;
+        //         case "up-qtt": ;
+        //         case "down-qtt": ;
+        //     }
+        // }
